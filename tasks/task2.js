194b80895d0076db1379292ba87456e1ac51d8c9
krrrDiana <152323478+krrrDiana@users.noreply.github.com>
2024-10-27 15:38:39 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -6,9 +6,43 @@
 
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
-
+const https = require('https');
 function createUser(user) {
   // Ваш код
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'jsonplaceholder.typicode.com',
+      path: '/users',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const req = https.request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        try {
+          const response = JSON.parse(data);
+          resolve(response);
+        } catch (error) {
+          reject('Помилка при обробці JSON');
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject('Помилка HTTP-запиту: ' + error.message);
+    });
+
+    req.write(JSON.stringify(user));
+    req.end();
+  });
 }
 
 console.log(createUser({name: "Sam", email: "fjsnfkjns2342@gmail.com"}))
