9cdc21b6ae0a545926061e4d8e2626af6046faaa
krrrDiana <152323478+krrrDiana@users.noreply.github.com>
2024-10-27 15:39:17 +0200

    Update task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -2,10 +2,45 @@
 "Поверніть статус відповіді сервера після видалення."
 
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
+const https = require('https');
 
 function deleteUser(id) {
-  // Ваш код
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'jsonplaceholder.typicode.com',
+      path: `/users/${id}`, 
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+    };
+
+    const req = https.request(options, (res) => {
+      const { statusCode } = res; 
+      res.on('data', () => {
+      });
+
+      res.on('end', () => {
+        if (statusCode >= 200 && statusCode < 300) {
+          resolve({
+            message: `Користувач з ID ${id} успішно видалений`, // Додано зворотні кавички
+            status: statusCode,
+          });
+        } else {
+          reject({
+            message: `Не вдалося видалити користувача з ID ${id}`, // Додано зворотні кавички
+            status: statusCode,
+          });
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject({ message: 'Помилка HTTP-запиту', error: error.message });
+    });
+
+    req.end();
+  });
 }
 
 console.log(deleteUser(1));
