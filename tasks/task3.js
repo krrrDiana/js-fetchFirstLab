032093785a4237222dac11b529e76e76703697ab
krrrDiana <152323478+krrrDiana@users.noreply.github.com>
2024-10-27 15:39:00 +0200

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -3,10 +3,43 @@
 "Поверніть відповідь від сервера з оновленими даними користувача."
 
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
+const https = require('https');
 
 function updateUser(id, updatedData) {
-  // Ваш код
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'jsonplaceholder.typicode.com',
+      path: `/users/${id}`,  
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const req = https.request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        try {
+          const response = JSON.parse(data);
+          resolve(response);
+        } catch (error) {
+          reject('Помилка при обробці JSON');
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject('Помилка HTTP-запиту: ' + error.message);
+    });
+
+    req.write(JSON.stringify(updatedData));
+    req.end();
+  });
 }
 
 console.log(updateUser(1, { name: 'New Name' }));
